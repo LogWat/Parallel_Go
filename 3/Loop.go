16e0c7b01d2bb7e123dae88e6711ac5f307eb729
LogWat <Nk8877wpt+Karan@gmail.com>
2021-12-01 17:14:36 +0900

    追加

---
+++ 3/Loop.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// ゴルーチンの意外性
+// 以下の場合，ゴルーチンが開始する前にループが終了する場合がほとんど
+// ゴルーチンはスコープ外からsalutationを参照する このとき，salutationは参照が
+// 残っているため，ヒープに移っている
+func main() {
+	var wg sync.WaitGroup
+	for _, salutation := range []string{"hello", "greetings", "good day"} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}()
+	}
+	wg.Wait()
+}
